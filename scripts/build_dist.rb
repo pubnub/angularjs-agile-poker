af6f0caa40ff9850eb2905881756149587ce6fed
Sunny Gleason <sunny.gleason@gmail.com>
2013-12-17 12:37:21 -0500

    fixup build script

--- scripts/build_dist.rb
+++ scripts/build_dist.rb
@@ -1,5 +1,7 @@
 #!/bin/env ruby
 
 `grunt clean build`
-`cp -r app/styles app/index.html .tmp/scripts .tmp/views dist/`
+`rm -rf dist`
+`mkdir dist`
+`cp -r app/styles app/index.html app/images .tmp/scripts .tmp/views dist/`
 
